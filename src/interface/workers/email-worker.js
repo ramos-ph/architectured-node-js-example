43df67c962f017626e89202ca74e03da171300fb
Pedro Ramos <pedrohsramos1110@gmail.com>
2024-12-15 19:07:30 -0300

    Add additional log

--- src/interface/workers/email-worker.js
+++ src/interface/workers/email-worker.js
@@ -1,9 +1,5 @@
 import { container } from "../../container.js";
 
-class EmailWorker {
-  static async sendWelcomeMail(job) {
-    await container.sendMail.execute(job.data);
-  }
-}
-
-export { EmailWorker };
+export const emailWorker = async (job) => {
+  await container.sendMail.execute(job.data);
+};
