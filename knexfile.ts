ef9863b86d630c18cf2c0f0d7fb875f5da7738d6
Pedro Ramos <pedrohsramos1110@gmail.com>
2025-04-08 16:47:35 -0300

    Add knexfile.ts

---
+++ knexfile.ts
@@ -0,0 +1,27 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      port: 5432,
+      user: "docker",
+      password: "docker",
+      database: "example_dev",
+    },
+  },
+
+  test: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      port: 5432,
+      user: "docker",
+      password: "docker",
+      database: "example_test",
+    },
+  },
+};
+
+export default config;
