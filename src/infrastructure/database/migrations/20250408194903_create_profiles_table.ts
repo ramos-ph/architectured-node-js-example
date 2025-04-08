ca033ebf660aca7589393a366b2411b42c9fbdad
Pedro Ramos <pedrohsramos1110@gmail.com>
2025-04-08 16:52:18 -0300

    Add create profiles migration

---
+++ src/infrastructure/database/migrations/20250408194903_create_profiles_table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("profiles", (t) => {
+    t.integer("id").primary();
+    t.string("username").notNullable();
+    t.string("email").notNullable().unique();
+    t.string("passwordHash").notNullable();
+    t.timestamps(true, true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("profiles");
+}
