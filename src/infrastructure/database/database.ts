ac0e35e91528c0c4113fee65024ce5af3a32ee62
Pedro Ramos <pedrohsramos1110@gmail.com>
2025-04-08 23:34:33 -0300

    Add database.ts

---
+++ src/infrastructure/database/database.ts
@@ -0,0 +1,20 @@
+import Knex from "knex";
+import knexfile from "../../../knexfile.ts";
+
+export const makeDatabase = () => {
+  const knex = Knex(knexfile[process.env.NODE_ENV || "development"]);
+
+  return {
+    knex,
+
+    async connect() {
+      await knex("SELECT 1");
+      console.log("Database connected successfully.");
+    },
+
+    async disconnect() {
+      await knex.destroy();
+      console.log("Database disconnected.");
+    },
+  };
+};
