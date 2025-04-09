13f9616f74a1c0f1fe9f221a84a57ba3c965120c
Pedro Ramos <pedrohsramos1110@gmail.com>
2025-04-08 23:35:07 -0300

    Add Knex related injectables

--- src/container.ts
+++ src/container.ts
@@ -4,18 +4,23 @@ import { PBKDF2PasswordHasher } from "./infrastructure/pbkdf2-password-hasher.js
 import { NodemailerMailerService } from "./infrastructure/mailers/nodemailer-mailer-service.js";
 import { SendMail } from "./application/use-cases/send-mail.js";
 import { BullMQQueueService } from "./infrastructure/bullmq-queue-service.js";
+import { makeDatabase } from "./infrastructure/database/database.ts";
+import { makeProfileRepositoryKnex } from "./infrastructure/repositories/profile-repository-knex.ts";
 
+const database = makeDatabase();
 const queueService = new BullMQQueueService({
   connection: { host: "localhost", port: 6379 },
 });
+
 const nodemailerMailerService = new NodemailerMailerService();
 const pbkdf2PasswordHasher = new PBKDF2PasswordHasher();
-const profileRepository = new ProfileRepositoryInMemory();
+const profileRepository = makeProfileRepositoryKnex(database.knex);
 
 const createProfile = new CreateProfile({ profileRepository, queueService });
 const sendMail = new SendMail({ mailerService: nodemailerMailerService });
 
 const container = {
+  database: database,
   queueService: queueService,
   profileRepository: profileRepository,
   createProfile: createProfile,
